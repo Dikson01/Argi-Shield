ca5a462f814f7130930538af80c5f8ba6acfa847
Dikson01 <diksonsharma2001@gmail.com>
2025-06-09 11:11:46 +0530

    commit database

--- Dep1/views.py
+++ Dep1/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Products
 
 # Create your views here.
 
-#when we want to just ahow this simple line of text without any html file
+#when we want to just show this simple line of text without any html file
 # def home(request):
 #   return HttpResponse("Welcome to Medihub OPD")
 
@@ -11,4 +12,5 @@ def index(request):
     return render(request,'dep1/index.html')
 
 def product(request):
-    return render(request,'dep1/product.html')
+    prods=Products.objects.all()
+    return render(request,'dep1/product.html',{'products' : prods})
